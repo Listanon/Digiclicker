443f462bae2b51650858ee1859f457461f04dcf8
Listanon <106467324+Listanon@users.noreply.github.com>
2022-05-31 05:59:10 +0300

    Update PokemonItem.ts

--- src/scripts/items/PokemonItem.ts
+++ src/scripts/items/PokemonItem.ts
@@ -33,3 +33,29 @@ class PokemonItem extends CaughtIndicatingItem {
         return `assets/images/items/${subDirectory}${this.name.replace(/[^\s\w.()-]/g, '')}.png`;
     }
 }
+
+ItemList['Eevee']                = new PokemonItem('Eevee', 4000);
+ItemList['Porygon']              = new PokemonItem('Porygon', 2000);
+ItemList['Jynx']                 = new PokemonItem('Jynx', 2000);
+ItemList['Mr. Mime']             = new PokemonItem('Mr. Mime', 1000);
+ItemList['Lickitung']            = new PokemonItem('Lickitung', 1000);
+ItemList['Magikarp']             = new PokemonItem('Magikarp', 50000, Currency.money);
+ItemList['Togepi']               = new PokemonItem('Togepi', 15000);
+ItemList['Beldum']               = new PokemonItem('Beldum', 22500);
+ItemList['Skorupi']              = new PokemonItem('Skorupi', 6750);
+ItemList['Combee']               = new PokemonItem('Combee', 6750);
+ItemList['Burmy (plant)']        = new PokemonItem('Burmy (plant)', 6750);
+ItemList['Cherubi']              = new PokemonItem('Cherubi', 6750);
+ItemList['Spiritomb']            = new PokemonItem('Spiritomb', 6750);
+ItemList['Zorua']                = new PokemonItem('Zorua', 50625);
+ItemList['Meloetta (pirouette)'] = new PokemonItem('Meloetta (pirouette)', 200000);
+ItemList['Furfrou (Debutante)']  = new PokemonItem('Furfrou (Debutante)', 1, Currency.healthcoin);
+ItemList['Furfrou (Diamond)']    = new PokemonItem('Furfrou (Diamond)', 1, Currency.speedcoin);
+ItemList['Furfrou (Matron)']     = new PokemonItem('Furfrou (Matron)', 1, Currency.attackcon);
+ItemList['Furfrou (Dandy)']      = new PokemonItem('Furfrou (Dandy)', 1, Currency.intcoin);
+ItemList['Furfrou (Kabuki)']     = new PokemonItem('Furfrou (Kabuki)', 1, Currency.defensecoin);
+ItemList['Furfrou (Pharaoh)']    = new PokemonItem('Furfrou (Pharaoh)', 1, Currency.magiccoin);
+ItemList['Type: Null']           = new PokemonItem('Type: Null', 114000);
+ItemList['Poipole']              = new PokemonItem('Poipole', 90000);
+ItemList['Eternatus']            = new PokemonItem('Eternatus', 50000);
+ItemList['Toxel']                = new PokemonItem('Toxel', 30000);
