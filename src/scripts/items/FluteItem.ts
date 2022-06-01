9ae842dffee99c03c878384325c1ffbf1d909f77
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-02 01:17:59 +0300

    Update FluteItem.ts

--- src/scripts/items/FluteItem.ts
+++ src/scripts/items/FluteItem.ts
@@ -66,15 +66,15 @@ class FluteItem extends Item {
 
 }
 
-ItemList['H_Fire_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Fire_Spirit, 'Click Attack', ['Fighting', 'Fire', 'Poison'], 'clickAttack', 1.02);
-ItemList['H_Light_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Light_Spirit, 'Exp Yield', ['Normal', 'Bug', 'Rock'], 'exp', 1.02);
-ItemList['H_Wind_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Wind_Spirit, 'Item Drop Rate', ['Normal', 'Flying', 'Poison'], undefined, 1.02);
-ItemList['H_Steel_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Steel_Spirit, 'Digidollar Yield', ['Dark', 'Electric', 'Steel'], 'money', 1.02);
-ItemList['H_Water_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Water_Spirit, 'Scan Coin Yield', ['Dark', 'Ghost', 'Ice'], 'dungeonToken', 1.02);
-ItemList['H_Thunder_Spirit']     = new FluteItem(GameConstants.FluteItemType.H_Thunder_Spirit, 'Digimon Attack', ['Fighting', 'Ice', 'Fairy'], 'pokemonAttack', 1.02);
-ItemList['Azure_Flute']          = new FluteItem(GameConstants.FluteItemType.Azure_Flute, 'Shiny Chance', ['Dragon', 'Ghost', 'Steel'], 'shiny', 1.02);
-ItemList['CH_Dark_Spirit']       = new FluteItem(GameConstants.FluteItemType.CH_Dark_Spirit, 'Roaming Chance', ['Flying', 'Dragon', 'Psychic'], 'roaming', 1.02);
-ItemList['H_Dark_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Dark_Spirit, 'Egg Steps', ['Fire', 'Ground', 'Water'], 'eggStep', 1.02);
-ItemList['H_Earth_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Earth_Spirit, 'Mining Energy Regeneration Yield and Time', ['Rock', 'Ground', 'Electric'], undefined, 1.02);
-ItemList['H_Ice_Spirit']         = new FluteItem(GameConstants.FluteItemType.H_Ice_Spirit, 'Dungeon Timer', ['Grass', 'Psychic', 'Water'], undefined, 1.02);
-ItemList['H_Wood_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Wood_Spirit, 'Farm Replant and Growth Multipliers', ['Grass', 'Bug', 'Fairy'], undefined, 1.02);
+ItemList['H_Fire_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Fire_Spirit, 'Click Attack', ['Fire', 'Vaccine', 'Electric'], 'clickAttack', 1.02);
+ItemList['H_Light_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Light_Spirit, 'Exp Yield', ['Light', 'Data', 'Neutral'], 'exp', 1.02);
+ItemList['H_Wind_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Wind_Spirit, 'Item Drop Rate', ['Plant', 'Wind', 'Vaccine'], undefined, 1.02);
+ItemList['H_Steel_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Steel_Spirit, 'Digidollar Yield', ['Free', 'Light', 'Neutral'], 'money', 1.02);
+ItemList['H_Water_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Water_Spirit, 'Scan Coin Yield', ['Free', 'Water', 'Dark'], 'dungeonToken', 1.02);
+ItemList['H_Thunder_Spirit']     = new FluteItem(GameConstants.FluteItemType.H_Thunder_Spirit, 'Digimon Attack', ['Vaccine', 'Fire', 'Electric'], 'pokemonAttack', 1.02);
+ItemList['Azure_Flute']          = new FluteItem(GameConstants.FluteItemType.Azure_Flute, 'Shiny Chance', ['Neutral', 'Earth', 'Dark'], 'shiny', 1.02);
+ItemList['CH_Dark_Spirit']       = new FluteItem(GameConstants.FluteItemType.CH_Dark_Spirit, 'Roaming Chance', ['Wind', 'Dark', 'Virus'], 'roaming', 1.02);
+ItemList['H_Dark_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Dark_Spirit, 'Egg Steps', ['Fire', 'Earth', 'Water'], 'eggStep', 1.02);
+ItemList['H_Earth_Spirit']       = new FluteItem(GameConstants.FluteItemType.H_Earth_Spirit, 'Mining Energy Regeneration Yield and Time', ['Data', 'Earth', 'Electric'], undefined, 1.02);
+ItemList['H_Ice_Spirit']         = new FluteItem(GameConstants.FluteItemType.H_Ice_Spirit, 'Dungeon Timer', ['Plant', 'Virus', 'Water'], undefined, 1.02);
+ItemList['H_Wood_Spirit']        = new FluteItem(GameConstants.FluteItemType.H_Wood_Spirit, 'Farm Replant and Growth Multipliers', ['Plant', 'Data', 'Light'], undefined, 1.02);
