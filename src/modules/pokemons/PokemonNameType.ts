5ff65849894774427f2f738900329d318381f15d
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-17 16:10:42 +0300

    Update PokemonNameType.ts

--- src/modules/pokemons/PokemonNameType.ts
+++ src/modules/pokemons/PokemonNameType.ts
@@ -745,7 +745,7 @@ export type PokemonNameType
     | 'Reppamon'
     | 'Sangloupmon'
     | 'Sealsdramon'
-    | 'Loader Liomon'
+    | 'Loader Leomon'
     | 'Matadrmon'
     | 'Metallife Kuwagamon'
     | 'Tankdramon'
