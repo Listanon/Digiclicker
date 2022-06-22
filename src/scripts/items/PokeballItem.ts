81ac94a90815466ecfd046277d0fe18f703a22dc
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-23 01:21:15 +0300

    Update PokeballItem.ts

--- src/scripts/items/PokeballItem.ts
+++ src/scripts/items/PokeballItem.ts
@@ -15,17 +15,17 @@ class PokeballItem extends Item {
 
 }
 
-ItemList['Pokeball']   = new PokeballItem(GameConstants.Pokeball.Pokeball, 100, undefined, undefined, 'Pokéball');
-ItemList['Greatball']  = new PokeballItem(GameConstants.Pokeball.Greatball, 500);
-ItemList['Ultraball']  = new PokeballItem(GameConstants.Pokeball.Ultraball, 2000);
-ItemList['Masterball'] = new PokeballItem(GameConstants.Pokeball.Masterball, 2500, GameConstants.Currency.farmPoint);
+ItemList['Pokeball']   = new PokeballItem(GameConstants.Pokeball.Pokeball, 100, undefined, undefined, 'Digiscan');
+ItemList['Greatball']  = new PokeballItem(GameConstants.Pokeball.Greatball, 500, undefined, undefined, 'Greatscan');
+ItemList['Ultraball']  = new PokeballItem(GameConstants.Pokeball.Ultraball, 2000, undefined, undefined, 'Ultrascan');
+ItemList['Masterball'] = new PokeballItem(GameConstants.Pokeball.Masterball, 2500, GameConstants.Currency.farmPoint, undefined, 'Masterscan');
 // Not sold in shops
-ItemList['Fastball'] = new PokeballItem(GameConstants.Pokeball.Fastball, Infinity, GameConstants.Currency.farmPoint);
-ItemList['Quickball'] = new PokeballItem(GameConstants.Pokeball.Quickball, Infinity, GameConstants.Currency.farmPoint);
-ItemList['Timerball'] = new PokeballItem(GameConstants.Pokeball.Timerball, Infinity, GameConstants.Currency.farmPoint);
-ItemList['Duskball'] = new PokeballItem(GameConstants.Pokeball.Duskball, Infinity, GameConstants.Currency.farmPoint);
-ItemList['Luxuryball'] = new PokeballItem(GameConstants.Pokeball.Luxuryball, Infinity, GameConstants.Currency.farmPoint);
-ItemList['Diveball'] = new PokeballItem(GameConstants.Pokeball.Diveball, Infinity, GameConstants.Currency.battlePoint);
-ItemList['Lureball'] = new PokeballItem(GameConstants.Pokeball.Lureball, Infinity, GameConstants.Currency.battlePoint);
-ItemList['Nestball'] = new PokeballItem(GameConstants.Pokeball.Nestball, Infinity, GameConstants.Currency.battlePoint);
-ItemList['Repeatball'] = new PokeballItem(GameConstants.Pokeball.Repeatball, Infinity, GameConstants.Currency.battlePoint);
+ItemList['Fastball'] = new PokeballItem(GameConstants.Pokeball.Fastball, Infinity, GameConstants.Currency.farmPoint, undefined, 'Fastscan');
+ItemList['Quickball'] = new PokeballItem(GameConstants.Pokeball.Quickball, Infinity, GameConstants.Currency.farmPoint, undefined, 'Quickscan');
+ItemList['Timerball'] = new PokeballItem(GameConstants.Pokeball.Timerball, Infinity, GameConstants.Currency.farmPoint, undefined, 'Timescan');
+ItemList['Duskball'] = new PokeballItem(GameConstants.Pokeball.Duskball, Infinity, GameConstants.Currency.farmPoint, undefined, 'Duskscan');
+ItemList['Luxuryball'] = new PokeballItem(GameConstants.Pokeball.Luxuryball, Infinity, GameConstants.Currency.farmPoint, undefined, 'Luxuryscan');
+ItemList['Diveball'] = new PokeballItem(GameConstants.Pokeball.Diveball, Infinity, GameConstants.Currency.battlePoint, undefined, 'Divescan');
+ItemList['Lureball'] = new PokeballItem(GameConstants.Pokeball.Lureball, Infinity, GameConstants.Currency.battlePoint, undefined, 'Lurescan');
+ItemList['Nestball'] = new PokeballItem(GameConstants.Pokeball.Nestball, Infinity, GameConstants.Currency.battlePoint, undefined, 'Nestscan');
+ItemList['Repeatball'] = new PokeballItem(GameConstants.Pokeball.Repeatball, Infinity, GameConstants.Currency.battlePoint, undefined, 'Repeatscan');
