bfac90f9a29a496265ea7775d19352e588b297d3
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-23 01:12:49 +0300

    Update CapturePokemonTypesQuest.ts

--- src/scripts/quests/questTypes/CapturePokemonTypesQuest.ts
+++ src/scripts/quests/questTypes/CapturePokemonTypesQuest.ts
@@ -42,7 +42,7 @@ class CapturePokemonTypesQuest extends Quest implements QuestInterface {
     }
 
     get description(): string {
-        return `Capture ${this.amount.toLocaleString('en-US')} ${PokemonType[this.type]} type Pokémon.`;
+        return `Scan ${this.amount.toLocaleString('en-US')} ${PokemonType[this.type]} type Digimon.`;
     }
 
     toJSON() {
