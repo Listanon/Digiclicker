5388bf9eafd98daedcdbd3fe99353da858a938a5
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-24 19:21:55 +0300

    Update TownList.ts

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -276,7 +276,7 @@ TownList['Pyokomon Village'] = new Town(
 TownList['Apocalymon Space'] = new Town(
     'Apocalymon Space',
     GameConstants.Region.kanto,
-    [GymList['Elite Lorelei'], GymList['Elite Bruno'], GymList['Elite Agatha'], GymList['Elite Lance'], GymList['Champion Blue'], pokeLeagueShop()],
+    [GymList['Ocean Data'], GymList['Forest Data'], GymList['City Data'], GymList['Wasteland Data'], GymList['Apokalymon'], pokeLeagueShop()],
     {
         requirements: [
             new RouteKillRequirement(10, GameConstants.Region.kanto, 23),
