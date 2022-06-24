e839a9167bede97309a44eb9084aea6aa2d46c09
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-25 01:10:37 +0300

    Update BadgeCaseController.ts

--- src/scripts/badgeCase/BadgeCaseController.ts
+++ src/scripts/badgeCase/BadgeCaseController.ts
@@ -7,7 +7,7 @@ class BadgeCaseController {
         GameConstants.RegionGyms.slice(0, region + 1).forEach((region, index) => {
             result[GameConstants.Region[index].charAt(0).toUpperCase() + GameConstants.Region[index].slice(1)] = region
                 .map(gym => BadgeEnums[GymList[gym].badgeReward])
-                .filter(b => !b.startsWith('Data') && b != 'None');
+                .filter(b => !b.endsWith('Data') && b != 'None');
         });
         return result;
     }
