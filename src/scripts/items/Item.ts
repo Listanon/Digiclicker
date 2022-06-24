832b9492195ce9ff64caa49438f63a25feae5db3
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-24 12:39:10 +0300

    Update Item.ts

--- src/scripts/items/Item.ts
+++ src/scripts/items/Item.ts
@@ -139,7 +139,7 @@ class Item {
                     break;
             }
             Notifier.notify({
-                message: `You don't have enough ${curr} for ${n} ${GameConstants.humanifyString(this.displayName) + multiple}`,
+                message: `You don't have enough ${curr} for that.`,
                 type: NotificationConstants.NotificationOption.danger,
             });
         }
