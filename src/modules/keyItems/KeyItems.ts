59ec50561986463f04986203689236fe20456f20
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-26 02:42:24 +0300

    Update KeyItems.ts

--- src/modules/keyItems/KeyItems.ts
+++ src/modules/keyItems/KeyItems.ts
@@ -84,7 +84,7 @@ export default class KeyItems implements Feature {
                 () => App.game.party.caughtPokemon.length >= 110, undefined, undefined, 'Holo Caster'),
             new KeyItem(KeyItemType.Mystery_egg, 'A mysterious Egg. This allows you to use the Digimon Day Care to help improve your Digimon attack. some baby Digimon can only be found through breeding too!',
                 () => App.game.statistics.routeKills[Region.adventure01][5]() >= ROUTE_KILLS_NEEDED, undefined, undefined, 'Mystery Egg'),
-            new KeyItem(KeyItemType.Safari_ticket, 'This ticket grants access to the Safari Zone right outside Fuchsia City.'),
+            new KeyItem(KeyItemType.Safari_ticket, 'This ticket grants access to the Safari Zone. Keep it, it might be useful later.'),
             new KeyItem(KeyItemType.Employee_Pass, 'Allows access to the Disk Shop',
                 () => MapHelper.accessToRoute(14, Region.adventure01), undefined, undefined, 'Employee Pass'),
 
