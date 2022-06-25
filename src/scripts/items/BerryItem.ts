380682bbc50aaf2039fa6e663c507b31d8b79197
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-25 04:40:23 +0300

    Update BerryItem.ts

--- src/scripts/items/BerryItem.ts
+++ src/scripts/items/BerryItem.ts
@@ -4,7 +4,7 @@ class BerryItem extends Item {
     public berryName: string;
 
     constructor(public berry: BerryType, basePrice: number, currency = GameConstants.Currency.farmPoint, public berryReq?: BerryType) {
-        super(`${BerryType[berry]}Berry`, basePrice, currency, { maxAmount: 1 } , undefined, `${BerryType[berry]} Berry`);
+        super(`${BerryType[berry]}Berry`, basePrice, currency, { maxAmount: 1 } , undefined, `${BerryType[berry]} Disk`);
         this.berryName = BerryType[berry];
     }
 
