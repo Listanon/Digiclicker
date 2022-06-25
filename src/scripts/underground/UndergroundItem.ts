912dcfe0a7a346c260100e27b0f7b0c90be96169
Listanon <106467324+Listanon@users.noreply.github.com>
2022-06-26 01:28:57 +0300

    Update UndergroundItem.ts

--- src/scripts/underground/UndergroundItem.ts
+++ src/scripts/underground/UndergroundItem.ts
@@ -89,7 +89,7 @@ UndergroundItem.addItem('Guilmon Bread',     3, [[1,1,1], [0,1,0]], 2);
 UndergroundItem.addItem('Broken Digivice',   4, [[1,1,1], [1,1,1], [1,1,1]], 4);
 UndergroundItem.addItem('Black Gear',        5, [[1,1,1], [1,1,1], [1,1,1]], 2);
 UndergroundItem.addItem('Digicores',         6, [[1,1], [1,0]], 10);
-UndergroundItem.addItem('Holy Ring',         7, [[1,1,1], [1,1,1], 6);
+UndergroundItem.addItem('Holy Ring',         7, [[1,1,1], [1,1,1]], 6);
 UndergroundItem.addItem('XAntibody',         8, [[1,1],[1,1]], 4);
 UndergroundItem.addItem('Empty Tag',         9, [[1,1,1,1], [1,1,1,1]], 3);
 UndergroundItem.addItem('Dreamy Digizoid',  10, [[0,0,1,0], [1,1,1,0], [0,1,1,1], [0,1,0,0]], 2);
